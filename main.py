7465ed0869f91b22dbc84b09e5c88f843968cbf0
akankshanehete <nehetea@mcmaster.ca>
2022-01-06 20:11:29 -0500

    got API to work successfully
    Signed-off-by: akankshanehete <nehetea@mcmaster.ca>

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import datetime
 APP_ID = '348bd446'
 API_KEY = '/////////////'
 
-# query = str(input("What did you do today?: "))
+query = str(input("What did you do today?: "))
 
 nutritionix_params = {
-    "query": "ran 3 miles",
+    "query": query,
     "gender": "female",
-    "weight_kg": 51,
-    "height_cm": 165,
-    "age": 19
+    "age": '19'
 }
 
 headers = {
@@ -20,9 +18,9 @@ headers = {
     "x-remote_user_id": '0'
 }
 
-nutritionix_response = requests.post(url='https://trackapi.nutritionix.com/v2/natural/exercise',
-                                    params=nutritionix_params,
+nutritionix_response = requests.post('https://trackapi.nutritionix.com/v2/natural/exercise',
+                                    json=nutritionix_params,
                                     headers=headers)
-print(nutritionix_response.text)
+
 exercise_data = nutritionix_response.json()
 print(exercise_data)
