3bd861b9779f10a0ff17c42c1479d69411a7821f
akankshanehete <nehetea@mcmaster.ca>
2022-01-06 20:50:42 -0500

    added natural language processing functionality
    Signed-off-by: akankshanehete <nehetea@mcmaster.ca>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 APP_ID = '348bd446'
-API_KEY = '/////////////'
+API_KEY = '////////////////'
 
 query = str(input("What did you do today?: "))
 
@@ -22,5 +22,24 @@ nutritionix_response = requests.post('https://trackapi.nutritionix.com/v2/natura
                                     json=nutritionix_params,
                                     headers=headers)
 
-exercise_data = nutritionix_response.json()
+exercise_data = nutritionix_response.json()['exercises']
 print(exercise_data)
+
+today = str(datetime.date.today().strftime("%d/%m/20%y"))
+current_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
+
+for exercise in exercise_data:
+    sheety_params = {
+        'workout' : {
+        'date' : today,
+        'time' : current_time,
+        'exercise' : exercise['user_input'].title(),
+        'duration' : exercise['duration_min'],
+        'calories' : exercise['nf_calories']
+        }
+    }
+    sheety_response = requests.post('https://api.sheety.co/ee9e56905dabfef98cb248dacc06b32a/myWorkouts/workouts',
+                                    json=sheety_params)
+print(sheety_response.text)
+
+
